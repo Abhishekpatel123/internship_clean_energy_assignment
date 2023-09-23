fd72cbdf45973f4717b038f006c10ccba0b8e065
Abhishek <abhipatel8719@gmail.com>
2023-09-24 02:56:08 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,140 +1,76 @@
-import { Button, Container, Grid, makeStyles, TextField } from "@material-ui/core";
-import { useEffect, useState } from "react";
-import Header from "./components/Header";
-import Card from "./components/Card";
-import { AddNewPost, DeletePost, GetAllPosts, UpdatePost } from './api/JsonPlaceholderData';
-
-const useStyles = makeStyles(theme => ({
-  left: {
-
-  },
-  right: {
-    background: theme.palette.grey[100],
-    boxShadow: '0 1px 3px gray',
-    height: "90vh",
-    // position: 'fixed',
-    // top: 0,
-    // right : 0
-    margin: '1rem 0',
-    borderRadius: '1rem'
-  },
-
-  containerGrid: {
-    [theme.breakpoints.down('md')]: {
-      // flexDirextion: 'column-reverse'
-      flexDirection: 'column-reverse',
-    }
-  }
-}))
-
+import * as React from "react";
+import { Box, Container, Text } from "@chakra-ui/react";
+import { ReactComponent as Logo } from "./assets/logo.svg";
+import { ReactComponent as Noti } from "./assets/noti.svg";
+import BG from "./assets/bg.png";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  const classes = useStyles()
-  const [posts, setPosts] = useState([]);
-  const [data, setData] = useState({
-    title: "",
-    body: ""
-  })
-
-  useEffect(() => {
-    GetAllPosts().then(data => {
-      if (data) setPosts(data)
-      console.log(data)
-    })
-  }, [])
-
-  const AddPost = (e) => {
-    e.preventDefault()
-    AddNewPost(data).then(data => {
-      setPosts(pre => ([data, ...pre]))
-    })
-  }
-  const Delete = (id) => {
-    console.log(id)
-
-    DeletePost(id).then(data => {
-      setPosts(pre => {
-        return pre.filter(ele => {
-          return ele.id !== id
-        })
-      })
-      console.log("delete", data)
-    })
-  }
-
-  const Update = (data)=>{
-    UpdatePost(data).then(data=>{
-      
-    })
-  }
-  const handleChange = (e) => setData(pre => ({ ...pre, [e.target.name]: e.target.value }))
-
   return (
-    <div className="App">
-
-      {/* header representing website  */}
-      <Header />
-
-      {/* all the main contained */}
-      <Container>
-        <Grid container spacing={8}
-          className={classes.containerGrid}
-        // classes={{ "direction-xs-column-reverse": classes.containerGrid }} 
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        margin: 10,
+        minHeight: "100vh",
+      }}
+    >
+      <Box style={{ width: 400, height: "100vh" }}>
+        <Box
+          style={{
+            backgroundImage: `url(${BG})`,
+            minHeight: "200px",
+            width: "100%",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
         >
-          <Grid className={classes.left} item xs={12} md={8}>
-            {
-              posts.map(card => (
-                <Card
-                  key={card.id}
-                  id={card.id}
-                  title={card.title}
-                  body={card.body}
-                  onDelete = {() => Delete(card.id)}
-                  onUpdate = {() => Update(card.id)}
+          <Container
+            py={2}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            {/* logo + company name */}
+            <Box display="flex" alignItems="center">
+              <Logo />
+              <Box ml={2}>
+                <Text fontSize="lg">Lifestyle</Text>
+                <Text fontSize="sm">Subtext</Text>
+              </Box>
+            </Box>
+            {/* notification */}
+            <Box bg="white" p={1} style={{ borderRadius: "50%" }}>
+              <Noti />
+            </Box>
+          </Container>
+        </Box>
 
-                />))
-            }
+        {/* middle  */}
+        <Container my={2}>
+          <Grid templateColumns="repeat(5, 1fr)" gap={1}>
+            <GridItem colSpan={4}>
+              {/* logo + company name */}
+              <Box display="flex" alignItems="center">
+                <Logo style={{ width: 100, hight: 100 }} />
+                <Box ml={2}>
+                  <Text fontSize="lg">Lifestyle</Text>
+                  <Text fontSize="sm">
+                    306, Pocket 5, Sector 2, Rohini, New Delhi, Delhi 110085
+                  </Text>
+                </Box>
+              </Box>
+            </GridItem>
+            <GridItem colSpan={1}>
+              <Text color="red"> 4 km</Text>
+            </GridItem>
           </Grid>
-          <Grid className={classes.right} item xs={12} md={4}>
-
-            <form onSubmit={AddPost} >
-              <TextField
-                variant="outlined"
-                name="title"
-                value={data.title}
-                label="Title"
-                color="secondary"
-                size="small"
-                fullWidth
-                required
-                onChange={handleChange}
-              />
-              <TextField
-                name="body"
-                variant="outlined"
-                value={data.value}
-                label="Body"
-                color="secondary"
-                size="small"
-                fullWidth
-                required
-                onChange={handleChange}
-                style={{
-                  margin: '1rem 0'
-                }}
-              />
-              <Button
-                type="submit"
-                style={{ fontWeight: 700 }}
-                fullWidth
-                color="secondary"
-                variant="contained" >
-                ADD POSTS
-            </Button>
-            </form>
-          </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </Box>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import { ThemeProvider } from '@material-ui/styles';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import theme from './DefaultTheme';
-import './index.css';
+import { ThemeProvider } from "@material-ui/styles";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import theme from "./DefaultTheme";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <React.StrictMode>
+  // <ThemeProvider theme={theme}>
+  <React.StrictMode>
+    <ChakraProvider>
       <App />
-    </React.StrictMode>
-  </ThemeProvider>,
-  document.getElementById('root')
+    </ChakraProvider>
+  </React.StrictMode>,
+  // </ThemeProvider>,
+  document.getElementById("root")
 );
